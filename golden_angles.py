e33290c3eec40f844126030ec18fb250d8aff0dd
Noemifc <noemifico@gmail.com>
2025-10-17 14:20:05 -0500

    Implement unique golden angle calculation and display
    Added functionality to remove duplicates and print unique golden angles.

--- golden_angles.py
+++ golden_angles.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# parametri
 end_angle = 360.
 # golden angle (mod 180) : tomo p
 golden_a = end_angle * (3 - np.sqrt(5)) / 2  # ≈ 111.246°
@@ -12,7 +13,7 @@ num_proj =100
 theta_start = np.array([
     13.76941013, 30., 40.0310562, 
 ])
-
+# golden angles interl
 golden_angles_tomo = np.mod(
     theta_start[:, None] + np.arange(num_proj) * golden_a,
     end_angle
@@ -23,9 +24,19 @@ print("10 golden angles:", golden_angles_tomo[:10])
 
 # send to PSO 
 golden_angles_tomo_sorted = np.sort(golden_angles_tomo)
-print("10 angles sorted:", golden_angles_tomo_sorted[:10])
-#----------------------------------------------------------
+
 # to add : sorted unique angles
+
+ # rimuove duplicati
+golden_angles_interl = np.unique(golden_angles_tomo_sorted)
+
+# --- STAMPA RISULTATI ---
+print("Totale angoli generati:", len(golden_angles_tomo))
+print("Totale angoli unici:", len(golden_angles_interl))
+print("Primi 10 angoli:", np.round(golden_angles_interl[:10], 3))
+print("\nTutti gli angoli (fino a 100):")
+print(np.round(golden_angles_interl[:100], 3))
+
 #----------------------------------------------------------
 
 # Plot 
@@ -37,4 +48,5 @@ for i, start in enumerate(theta_start):
     plt.polar(np.deg2rad(angles), np.ones_like(angles), '.', alpha=0.6, color=colors[i])
 
 plt.title("Interlaced golden-angle sampling (mod %.1f°)" % end_angle)
+plt.legend()
 plt.show()
